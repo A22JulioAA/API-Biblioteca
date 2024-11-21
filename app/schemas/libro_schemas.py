65d891c7193fd5209d3f820392b8e82cc126fe51
a22julioaa <a22julioaa@iessanclemente.net>
2024-11-21 21:27:11 +0100

    feature(schemas): creados esquemas de Libro

---
+++ app/schemas/libro_schemas.py
@@ -0,0 +1,44 @@
+from pydantic import BaseModel
+
+class LibroBase(BaseModel):
+    isbn: str
+    titulo: str
+    autor: str
+    descripcion: str
+    editorial: str
+    genero: str
+    pais: str
+    idioma: str
+    num_paginas: int
+    year_edicion: int
+    precio: float
+
+class LibroCreate(LibroBase):
+    pass
+
+class LibroUpdate(LibroBase):
+    pass
+
+class Libro(LibroBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+class LibroResponse(LibroBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+class LibroInDB(LibroBase):
+    id: int
+    created_at: str
+    updated_at: str
+
+    class Config:
+        orm_mode = True
+
+
+
+
