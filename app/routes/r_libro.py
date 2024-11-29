0f216b4cc92b634668d256bb9150280fc30b4da8
xulio <a22julioaa@iessanclemente.net>
2024-11-30 00:07:51 +0100

    feature(routers libros): añadir rutas para leer libros y empezar el endpoint de añadir libro

--- app/routes/r_libro.py
+++ app/routes/r_libro.py
@@ -4,9 +4,10 @@ from datetime import datetime
 from fastapi import APIRouter, HTTPException, Depends
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import SQLAlchemyError
 
 from models.libro import Libro
-from schemas.libro_schemas import LibroResponse
+from schemas.libro_schemas import LibroResponse, LibroCreate
 
 from database import get_db
 
@@ -15,6 +16,7 @@ libros_router = APIRouter(
     tags=['Libros']
 )
 
+# Ruta para obtener todos los libros
 @libros_router.get(
     '/',
     description='Obtener todos los libros',
@@ -25,11 +27,156 @@ libros_router = APIRouter(
         },
         404: {
             'description': 'No hay libros registrados'
+        },
+        500: {
+            'description': 'Error del servidor'
         }
     }
 )
 async def get_libros(db: Session = Depends(get_db)):
-    libros = db.query(Libro).all()
-    if libros:
-        return libros
-    raise HTTPException(status_code=404, detail='No hay libros registrados')
+    try: 
+        # Consultamos los libros de la base de datos. Si no hay libros, se lanza una excepción
+        libros = db.query(Libro).all()
+
+        # Si hay libros, los devolvemos. Si no, lanzamos una excepción
+        if libros:
+            return libros
+        else:
+            raise HTTPException(status_code=404, detail='No hay libros registrados')
+        
+    except SQLAlchemyError as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+# Ruta para obtener un libro por su ID
+@libros_router.get(
+    '/{id}',
+    description='Obtener un libro por su ID',
+    responses={
+        200: {
+            'description': 'Libro encontrado',
+            'model': LibroResponse
+        },
+        404: {
+            'description': 'Libro no encontrado'
+        },
+        500: {
+            'description': 'Error del servidor'
+        }
+    }
+)
+async def get_libro_by_id(id: int, db: Session = Depends(get_db)):
+    try:
+        # Consultamos el libro por su ID
+        libro = db.query(Libro).filter(Libro.id == id).first()
+
+        # Si el libro existe, lo devolvemos. Si no, lanzamos una excepción
+        if libro:
+            return libro
+        else:
+            raise HTTPException(status_code=404, detail='Libro no encontrado')
+
+    except SQLAlchemyError as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+# Ruta para obtener un libro por su ISBN
+@libros_router.get(
+    '/isbn/{isbn}',
+    description='Obtener un libro por su ISBN',
+    responses={
+        200: {
+            'description': 'Libro encontrado',
+            'model': LibroResponse
+        },
+        404: {
+            'description': 'Libro no encontrado'
+        },
+        500: {
+            'description': 'Error del servidor'
+        }
+    }
+)
+async def get_libro_by_isbn(isbn: str, db: Session = Depends(get_db)):
+    try:
+        # Comprobamos que el ISBN tenga 13 caracteres TODO: Podría hacerse una validación de ISBN de 10 dígitos
+        if len(isbn) != 13:
+            raise HTTPException(status_code=400, detail='El ISBN debe tener 13 caracteres')
+        
+        # Consultamos el libro por su ISBN
+        libro = db.query(Libro).filter(Libro.isbn == isbn).first()
+
+        # Si el libro existe, lo devolvemos. Si no, lanzamos una excepción
+        if libro:
+            return libro
+        else:
+            raise HTTPException(status_code=404, detail='Libro no encontrado')
+
+    except SQLAlchemyError as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+# Ruta para obtener todos los libros de un autor
+@libros_router.get(
+    '/autor/{autor}',
+    description='Obtener todos los libros de un autor',
+    responses={
+        200: {
+            'description': 'Lista de libros',
+            'model': LibroResponse
+        },
+        404: {
+            'description': 'No hay libros del autor'
+        },
+        500: {
+            'description': 'Error del servidor'
+        }
+    }
+)
+async def get_libros_by_autor(autor: str, db: Session = Depends(get_db)):
+    try:
+        # Consultamos los libros por el autor
+        libros = db.query(Libro).filter(Libro.autor == autor).all()
+
+        # Si hay libros, los devolvemos. Si no, lanzamos una excepción
+        if libros:
+            return libros
+        else:
+            raise HTTPException(status_code=404, detail='No hay libros del autor')
+
+    except SQLAlchemyError as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+# Ruta para añadir un libro
+@libros_router.post(
+    '/',
+    description='Añadir un libro',
+    responses={
+        201: {
+            'description': 'Libro añadido',
+            'model': LibroResponse
+        },
+        400: {
+            'description': 'Datos incorrectos'
+        },
+        409: {
+            'description': 'El libro ya existe'
+        },
+        500: {
+            'description': 'Error del servidor'
+        }
+    }
+)
+async def add_libro(libro: LibroCreate, db: Session = Depends(get_db)):
+    try:
+        # Comprobamos que no haya un libro con el mismo ISBN
+        if db.query(Libro).filter(Libro.isbn == libro.isbn).first():
+            raise HTTPException(status_code=409, detail=f'El libro con el ISBN - ${libro.isbn} - ya existe')
+
+        # Añadimos el libro a la base de datos
+        db.add(libro)
+        db.commit()
+
+        return libro
+
+    except SQLAlchemyError as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+    
--- app/schemas/libro_schemas.py
+++ app/schemas/libro_schemas.py
@@ -15,7 +15,17 @@ class LibroBase(BaseModel):
     precio: float
 
 class LibroCreate(LibroBase):
-    pass
+    isbn: str = "9906453786057"
+    titulo: str = "El señor de los anillos"
+    autor: str = "J.R.R. Tolkien"
+    descripcion: str = "Un anillo para gobernarlos a todos, un anillo para encontrarlos, un anillo para atraerlos a todos y atarlos en las tinieblas."
+    editorial: str = "Minotauro"
+    genero: str = "Fantasía"
+    pais: str = "Reino Unido"
+    idioma: str = "Español"
+    num_paginas: int = 1216
+    ano_edicion: int = 1954
+    precio: float = 25.95
 
 class LibroUpdate(LibroBase):
     pass
